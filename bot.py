f3be1cb6a5086e6f73023696ff2293e4ec3d104f
sweetenpotato <rayme18@naver.com>
2020-08-24 01:34:14 +0900

    Initial Booting Message Added

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+# discord.py 모듈 import
 import discord
+
+# 토큰
+token = ""
+
+# 클라이언트
+client = discord.Client()
+
+# 봇이 작동 시작 시 console에 Log in 출력
+@client.event
+async def on_ready():
+    print("Log in")
